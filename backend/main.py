5d7c1030407e4254a869b502f4530d46be42885c
rafih268 <rafi.h2682002@gmail.com>
2025-07-10 17:47:23 +0100

    feat(backend): update parsing implementation to now retrieve object and data properties

--- backend/main.py
+++ backend/main.py
@@ -16,8 +16,8 @@ app.add_middleware(
 
 ontology_data = {
   "classes": [],
-  "instances": [],
-  "relations":[]
+  "object_properties": [],
+  "data_properties": []
 }
 
 @app.post("/upload-ontology")
@@ -30,13 +30,39 @@ async def upload_ontology(file: UploadFile = File()):
   
   onto = get_ontology(f"file://{tmp_path}").load()
   
-  classes = [cls.name for cls in onto.classes()]
-  instances = [inst.name for inst in onto.individuals()]
-  relations = [(prop.name, prop.domain, prop.range) for prop in onto.object_properties()]
+  # classes = [cls.name for cls in onto.classes()]
+  # instances = [inst.name for inst in onto.individuals()]
+  # relations = [(prop.name, prop.domain, prop.range) for prop in onto.object_properties()]
+
+  classes = []
+  for cls in onto.classes():
+    classes.append({
+      "name": cls.name
+    })
+  
+  object_props = []
+  for prop in onto.object_properties():
+    domains = [d.name for d in prop.domain]
+    ranges = [r.name for r in prop.range]
+    object_props.append({
+      "name": prop.name,
+      "domain": prop.domains,
+      "range": prop.ranges
+    })
+  
+  data_props = []
+  for prop in onto.data_properties():
+    domains = [d.name for d in prop.domain]
+    ranges = [str(r) for r in prop.range]
+    data_props.append({
+      "name": prop.name,
+      "domain": domains,
+      "range": ranges,
+    })
 
   ontology_data["classes"] = classes
-  ontology_data["instances"] = instances
-  ontology_data["relations"] = [(str(p), str(d), str(r)) for p, d, r in relations]
+  ontology_data["object_properties"] = object_props
+  ontology_data["data_properties"] = data_props
 
   os.remove(tmp_path)
   return {"status": "uploaded"}

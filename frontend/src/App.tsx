617407dc2fcba5c6794940cf813287eb87129f18
rafih268 <rafi.h2682002@gmail.com>
2025-08-04 21:06:20 +0100

    feat: implement footer component and add to the main application

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,25 @@
 import Navbar from './components/Navbar';
 import Upload from './components/Upload'
 import DiagramCanvas from './components/DiagramCanvas'
-import ClassTreeSidebar from './components/ClassTreeSidebar'
+import Footer from './components/Footer';
 
 export default function App() {
   return (
-    <div className="bg-gray-50 flex flex-col min-h-screen">
-      <Navbar />
-      <div className="flex flex-1 items-start justify-center p-4 space-x-4 max-w-7xl mx-auto">
-        <div className="p-4 rounded shadow h-[600px] bg-blue-50">
-          <h2 className="text-xl font-bold">Class Tree</h2>
-          <ClassTreeSidebar />
-        </div>
-        <div className="flex-1">
-          <DiagramCanvas />
-        </div>
-
-        <div className="p-4 rounded shadow bg-blue-50">
-          <h2 className="text-xl font-bold">Upload Your Ontology</h2>
-          <Upload />
+    <div>
+      <div className="bg-gray-50 flex flex-col min-h-screen">
+        <Navbar />
+        <div className="flex flex-1 items-start justify-center p-4 space-x-4 max-w-7xl mx-auto">
+          <div className="flex-1">
+            <DiagramCanvas />
+          </div>
+  
+          <div className="p-4 rounded shadow bg-blue-50 h-fit">
+            <h2 className="text-xl font-bold">Upload Your Ontology</h2>
+            <Upload />
+          </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 }

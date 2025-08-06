57809dec469adb4a19dd44baebfdd84ec69265b1
rafih268 <rafi.h2682002@gmail.com>
2025-08-06 13:04:34 +0100

    chore: remove unused file

--- frontend/src/components/ClassTreeSidebar.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ClassTreeSidebar() {
-  return (
-    <aside className="w-64 p-4">
-      {/* Code for rendering the class hierarchy retrieved from ontology */}
-    </aside>
-  );
-}
